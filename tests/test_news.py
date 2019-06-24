665f43aa2601f3f31785ee332079979cb40ed1db
Lugaga <maureezgaga@gmail.com>
2019-06-24 09:26:52 +0300

    Update test_news.py

--- tests/test_news.py
+++ tests/test_news.py
@@ -33,16 +33,16 @@ class ArticlesTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article = Articles('CNN','pricilla Gumato','The tech scene in Africa-Is it the next big thing?','A look at various tech hubs in Africa and the impact they have on the worlds economy','techie.com','techie.com/7643t94.jpg','2019-04-14')
+        self.new_article = Articles('CNN','lg','The tech scene in Africa-Is it the next big thing?','A look at various tech hubs in Africa and the impact they have on the worlds economy','techie.com','techie.com/lg.jpg','2019-04-14')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Articles))
 
     def test_to_check_instance_variables(self):
         self.assertEquals(self.new_article.id,'CNN')
-        self.assertEquals(self.new_article.author,'pricilla Gumato')
+        self.assertEquals(self.new_article.author,'lg')
         self.assertEquals(self.new_article.title,'The tech scene in Africa-Is it the next big thing?')
         self.assertEquals(self.new_article.description,'A look at various tech hubs in Africa and the impact they have on the worlds economy')
-        self.assertEquals(self.new_article.url,'techie.com')
-        self.assertEquals(self.new_article.image,'techie.com/7643t94.jpg')
-        self.assertEquals(self.new_article.date,'2019-04-14')
+        self.assertEquals(self.new_article.url,'lg.com')
+        self.assertEquals(self.new_article.image,'lg.com/lg.jpg')
+        self.assertEquals(self.new_article.date,'2019-06-23')
